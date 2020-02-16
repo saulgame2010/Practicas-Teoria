a3cfc4d1d8b056bba0ffd3343b11b2c20d367de2
saulgame2010 <35746616+saulgame2010@users.noreply.github.com>
2020-02-16 13:36:18 -0600

    Version 1.0.3
    Ya se arregló el fondo y se agregaron detalles estéticos

--- Expresiones_regulares/src/expresiones_regulares/ConexionBD.java
+++ Expresiones_regulares/src/expresiones_regulares/ConexionBD.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package expresiones_regulares;
 
 import java.sql.*;
@@ -10,7 +6,7 @@ import javax.swing.*;
 
 public class ConexionBD {
     public String bd = "Expresiones";
-	public String url = "jdbc:mysql://localhost:3307/" + bd;
+	public String url = "jdbc:mysql://localhost:3306/" + bd;
 	public String usuario = "root";
 	public String pass = "";
 

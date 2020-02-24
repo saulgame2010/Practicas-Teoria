322f2a0cee658bec804416b3c0848d76b9a6da9b
ulisesJ01 <56226569+ulisesJ01@users.noreply.github.com>
2020-02-23 22:50:50 -0600

    1.0.3
    solo falta el ultimo inciso

--- practica2.py
+++ practica2.py
@@ -1,10 +1,19 @@
 import random
 from posixpath import join
 import itertools
+import re
 
 alfabeto=[]
 lenguaje1=[]
 lenguaje2=[]
+nombresh=["Ulises","Saul","Rafael","Jaime","Gabriel","Armando","Martin","Daniel"]
+nombresm=["Alejandra","Dalila","Mariela","Gabriela","Blanca","Carolina","Monserrat"]
+apellidosp=["Juarez","Garcia","Medina","Palacios","Alvarez","Hernandez","Esquivel","Lara","Chavez","Angel"]
+apellidosm=["Espinoza","Pantoja","Doroteo","Castrejon","Lujan","Martinez","Rueda","Martinez","Gutierrez"]
+estados= ["AS","BC","BS","CC","CS","CH","CL","CM","DF","DG","GT","GR","HG","JC","MC","MN","MS","NT","NL","OC","PL","QO","QR","SP","SL","SR","TC","TS","TL","VZ","YN","ZS"]
+dia=["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]
+mes=["01","02","03","04","05","06","07","08","09","10","11","12"]
+digitos=["0","1","2","3","4","5","6","7","8","9"]
 def alf(alfa):
 	while True:
 		print("¿Como quieres ingresar tu alfabeto?\n1.-Por extension\n2.-Por rango")
@@ -85,13 +94,12 @@ def elevarlen(n,lenguaje,base):
 		n=n*-1
 	if n==0:
 		print("")
-		return
 	if n>1:
 		for palabra in lenguaje:
-			elevarlen((n-1),lenguaje,palabra,end=",")
+			elevarlen((n-1),lenguaje,palabra +str(base))
 	else:
 		for palabra in lenguaje:
-			print(base+str(palabra),end=",")
+			print(base + str(palabra),end=" ")
 
 def eleselec():
 	print("""\n\t---Elevando lenguaje---
@@ -118,7 +126,29 @@ def eleselec():
 			elevarlen(potencia,lenguaje1,"")
 	if(numero==2):
 			elevarlen(potencia,lenguaje2,"")
-
+def curp_ale(): 
+	regexVocal = re.compile('a|e|i|o|u|A|E|I|O|U')
+	regexConsonante=re.compile('B|C|D|F|G|H|J|K|L|M|N|Ñ|P|Q|R|S|T|V|W|X|Y|Z|b|c|d|f|g|h|j|k|l|m|n|ñ|p|q|r|s|t|v|w|x|y|z')
+	nom=random.choice(nombresh)
+	nomm=random.choice(nombresm)
+	ap=random.choice(apellidosp)
+	am=random.choice(apellidosm)
+	conap=regexConsonante.findall(ap[1::])[0]
+	conam=regexConsonante.findall(am[1::])[0]
+	connom=regexConsonante.findall(nom[1::])[0]
+	apm=random.choice(apellidosp)
+	amm=random.choice(apellidosm)
+	conapm=regexConsonante.findall(apm[1::])[0]
+	conamm=regexConsonante.findall(amm[1::])[0]
+	connomm=regexConsonante.findall(nomm[1::])[0]
+	auxaño=[]
+	for año in range(1900,2019):
+		auxaño.append(str(año))
+	curph=ap[0]+regexVocal.findall(ap)[0]+am[0]+nom[0]+random.choice(auxaño)[::-1][0:2][::-1] +random.choice(mes) +random.choice(dia)+"H"+random.choice(estados)+conap+conam+connom+random.choice(digitos)+random.choice(digitos)
+	curpm=apm[0]+regexVocal.findall(apm)[0]+amm[0]+nomm[0]+random.choice(auxaño)[::-1][0:2][::-1] +random.choice(mes) +random.choice(dia)+"M"+random.choice(estados)+conapm+conamm+connomm+random.choice(digitos)+random.choice(digitos)
+	print("Curp de hombre: ",str(curph.upper()))
+	print("Curp de mujer: ",str(curpm.upper()))
+	print(str(auxaño))
 #a
 alf(alfabeto)
 #b
@@ -137,8 +167,19 @@ print("\n\t---Diferencia de lenguajes---")
 restalen(lenguaje1,lenguaje2)
 #f
 eleselec()
-print("\n")
-eleselec()
-
+print("""\nDesea repetir?
+	1.-Si
+	2.-No""")
+validar=int(input("Selección: "))
+while validar!=2:
+	eleselec()
+	print("""\n\nDesea repetir?
+	1.-Si
+	2.-No""")
+	validar=int(input("Selección: "))
+#g
+	
+print("\nGenerando 2 curp aleatorias: ")
+curp_ale()
 
 

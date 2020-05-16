01938c9e3a2c4561c265ee2a3014a385c8835703
saulgame2010 <35746616+saulgame2010@users.noreply.github.com>
2020-05-16 17:11:18 -0500

    v1.0.1
    Esta es la versión de respaldo

--- practica4.py
+++ practica4.py
@@ -1,9 +1,13 @@
+import xlrd
+
 tabla = []
 alfabeto = []
 states = []
 c0 = set()
 c1 = set()
+cn = set()
 c = []
+b = []
 print("Ingresa el numero de elementos de tu alfabeto\n")
 numE = int(input())
 print("Ingresa los simbolos de tu alfabeto\n")
@@ -22,16 +26,34 @@ for s in range(0, nc0):
 for j in states:
 	if(j not in c0):
 		c1.add(j)
-c.append(c0)
-c.append(c1)
+ck = list(c0)
+cj = list(c1)
+c.append(ck)
+c.append(cj)
 for i in range(0, nstates):
 	tabla.append([0]*numE)
-print(tabla)
 print("Introduce la tabla de transicion del AFD")
 for f in range(0, nstates):
 	for col in range(0, numE):
 		print("Estado ",states[f]," simbolo ",alfabeto[col])
 		tabla[f][col] = input()
 
-print(tabla)
+
+"""while True:
+	if(nc0 == 1 and len(cj) > 1):
+		for f in range(0, len(cj)):
+			for col in range(0, numE):
+				if(tabla[f][col] in cj):					
+					cn.add(tabla[f][col])
+					cj.remove(tabla[f][col])
+	elif(nc0 > 1 and len(cj) == 1):
+		for f in range(0, len(ck)):
+			for col in range(0, numE):
+				if(tabla[f][col] in ck):					
+					cn.add(tabla[f][col])
+					ck.remove(tabla[f][col])
+	cm = list(cn)
+	c = [ck, cj, cm]
+	break"""				
+#print(tabla)
 print(c)
